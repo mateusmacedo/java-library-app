885484e8a3d722c9e9c52e13fb66aae5c26af919
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-05-30 10:32:32 -0300

    feat(BookRegistration): :sparkles: enhanced book registration function
    The book registration process in the library domain service has been enhanced. Now, the method `registerBook` instead of returning an Object now returns an instance of Book after validating the incoming data and persisting the new book to the repository. This refinement makes the function more specific and coherent and delegates the responsibility of persisting the book to the appropriate repository.

--- src/main/java/com/mmda/library/domain/service/BookRegistration.java
+++ src/main/java/com/mmda/library/domain/service/BookRegistration.java
@@ -1,20 +1,32 @@
 package com.mmda.library.domain.service;
 
+import com.mmda.core.repository.PersistenceRepository;
 import com.mmda.core.validation.ValidationException;
 import com.mmda.core.validation.Validator;
 import com.mmda.library.domain.dto.BookRegistrationDto;
+import com.mmda.library.domain.model.Book;
 
 public class BookRegistration {
     private final Validator<BookRegistrationDto> validator;
+    private final PersistenceRepository<Book> repository;
 
-    public BookRegistration(Validator<BookRegistrationDto> validator) {
+    public BookRegistration(
+        Validator<BookRegistrationDto> validator,
+        PersistenceRepository<Book> repository
+        ) {
         this.validator = validator;
+        this.repository = repository;
     }
 
-    public Object registerBook(BookRegistrationDto dto) throws ValidationException {
-        Object result = new Object();
+    public Book registerBook(BookRegistrationDto dto) throws ValidationException {
         validator.validate(dto);
-
-        return result;
+        Book book = new Book(
+            dto.getTitle(),
+            dto.getAuthors(),
+            dto.getIsbn(),
+            dto.getCategories(),
+            dto.getPublicationYear()
+        );
+        return repository.save(book);
     }
 }
