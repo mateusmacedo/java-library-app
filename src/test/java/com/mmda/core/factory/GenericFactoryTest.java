b08868c18ee5cd267d0303b97058d913dff67125
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-05-30 13:33:40 -0300

    test(GenericFactory): :test_tube: added tests for GenericFactory class
    We now verify:
    1. Object creation with correct parameters.
    2. Handling of FactoryException with incorrect parameters.
    3. Proper response to no matching constructors.

---
+++ src/test/java/com/mmda/core/factory/GenericFactoryTest.java
@@ -0,0 +1,75 @@
+package com.mmda.core.factory;
+
+import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GenericFactoryTest {
+
+    static class TestClass {
+        private final int number;
+        private final String text;
+
+        public TestClass(int number, String text) {
+            this.number = number;
+            this.text = text;
+        }
+
+        public int getNumber() {
+            return number;
+        }
+
+        public String getText() {
+            return text;
+        }
+    }
+
+    private GenericFactory<TestClass> factory;
+
+    private FactoryException exception;
+
+    @BeforeEach
+    public void setUp() {
+        factory = new GenericFactory<>(TestClass.class);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        factory = null;
+        exception = null;
+    }
+
+    @Test
+    public void create_shouldReturnInstance_whenParametersAreCorrect() {
+        // Act
+        TestClass instance = factory.create(123, "Hello World");
+
+        // Assert
+        assertNotNull(instance);
+        assertEquals(123, instance.getNumber());
+        assertEquals("Hello World", instance.getText());
+    }
+
+    @Test
+    public void create_shouldReturnFactoryExceptions_whenIncorrectParameters() {
+        // Arrange and Act
+        exception = assertThrows(FactoryException.class, () -> factory.create("Hello World", 123));
+
+        // Assert
+        assertNotNull(exception);
+        assertEquals("Error creating instance of com.mmda.core.factory.GenericFactoryTest$TestClass", exception.getMessage());
+    }
+
+    @Test
+    public void create_shouldReturnFactoryException_whenNoMatchingConstructor() {
+        // Arrange and Act
+        exception = assertThrows(FactoryException.class, () -> factory.create(123));
+
+        // Assert
+        assertNotNull(exception);
+        assertEquals("No matching constructor found for parameters", exception.getMessage());
+    }
+}
