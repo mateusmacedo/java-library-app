70f6bf9cdd493a9e3ef98e476c878bba151c8887
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-05-30 10:48:44 -0300

    test(BookRegistrationTest): :test_tube: migrated tests to JUnit 5 and refactored code for efficiency
    This commit transitions the testing framework from JUnit 4 to JUnit 5 in `BookRegistrationTest`. It also optimizes the testing code by simplifying assertions and eliminating unnecessary whitespace. Now, it ensures that tests are up-to-date with the latest JUnit version and are more readable and succinct, improving maintainability.

--- src/test/java/com/mmda/library/domain/service/BookRegistrationTest.java
+++ src/test/java/com/mmda/library/domain/service/BookRegistrationTest.java
@@ -2,19 +2,21 @@ package com.mmda.library.domain.service;
 
 import java.util.HashSet;
 
-import org.junit.After;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import static org.mockito.ArgumentMatchers.any;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.mmda.core.repository.PersistenceRepository;
 import com.mmda.core.validation.ValidationException;
@@ -25,6 +27,7 @@ import com.mmda.library.domain.model.Book;
 import com.mmda.library.domain.valueobject.Category;
 import com.mmda.library.domain.valueobject.Isbn;
 
+@ExtendWith(MockitoExtension.class)
 public class BookRegistrationTest {
 
     @Mock
@@ -33,15 +36,15 @@ public class BookRegistrationTest {
     @Mock
     private PersistenceRepository<Book> repository;
 
+    @InjectMocks
     private BookRegistration bookRegistration;
 
-    @Before
+    @BeforeEach
     public void setUp() {
-        MockitoAnnotations.openMocks(this);
         bookRegistration = new BookRegistration(validator, repository);
     }
 
-    @After
+    @AfterEach
     public void tearDown() {
         validator = null;
         repository = null;
@@ -51,78 +54,51 @@ public class BookRegistrationTest {
     public void registerBook_shouldReturnValidationException_whenDtoIsInvalid() {
         String title = "Title";
         Author author = new Author("Name", "Surname");
-
         HashSet<Author> authors = new HashSet<>();
         authors.add(author);
-
         Isbn isbn = new Isbn("1234567890123");
         Category category = new Category("Category");
-
         HashSet<Category> categories = new HashSet<>();
         categories.add(category);
-
         Integer publicationYear = 2021;
 
         BookRegistrationDto dto = new BookRegistrationDto(
-                title,
-                authors,
-                isbn,
-                categories,
-                publicationYear);
+                title, authors, isbn, categories, publicationYear);
 
         doThrow(new ValidationException("Invalid DTO")).when(validator).validate(dto);
 
-        ValidationException exception = assertThrows(ValidationException.class, () -> {
-            bookRegistration.registerBook(dto);
-        });
+        ValidationException exception = assertThrows(ValidationException.class,
+                () -> bookRegistration.registerBook(dto));
 
-        assert exception.getMessage().equals("Invalid DTO");
-        verify(validator, times(1)).validate(dto);
-        verify(repository, times(0)).save(any(Book.class));
+        assertEquals("Invalid DTO", exception.getMessage());
+        verify(validator).validate(dto);
+        verify(repository, never()).save(any(Book.class));
     }
 
     @Test
     public void registerBook_shouldReturnBook_whenDtoIsValid() {
         String title = "Title";
         Author author = new Author("Name", "Surname");
-
         HashSet<Author> authors = new HashSet<>();
         authors.add(author);
-
         Isbn isbn = new Isbn("1234567890123");
         Category category = new Category("Category");
-
         HashSet<Category> categories = new HashSet<>();
         categories.add(category);
-
         Integer publicationYear = 2021;
 
         Book expectedBook = new Book(
-            title,
-            authors,
-            isbn,
-            categories,
-            publicationYear
-        );
+                title, authors, isbn, categories, publicationYear);
         BookRegistrationDto dto = new BookRegistrationDto(
-            title,
-            authors,
-            isbn,
-            categories,
-            publicationYear);
+                title, authors, isbn, categories, publicationYear);
 
-        doNothing().when(validator).validate(dto);
+        doNothing().when(validator).validate(any(BookRegistrationDto.class));
         when(repository.save(any(Book.class))).thenReturn(expectedBook);
 
         Book resultBook = bookRegistration.registerBook(dto);
 
-        assertEquals(expectedBook.getTitle(), resultBook.getTitle());
-        assertEquals(expectedBook.getAuthors(), resultBook.getAuthors());
-        assertEquals(expectedBook.getIsbn(), resultBook.getIsbn());
-        assertEquals(expectedBook.getCategories(), resultBook.getCategories());
-        assertEquals(expectedBook.getPublicationYear(), resultBook.getPublicationYear());
-
-        verify(validator, times(1)).validate(dto);
-        verify(repository, times(1)).save(any(Book.class));
+        assertEquals(expectedBook, resultBook);
+        verify(validator).validate(dto);
+        verify(repository).save(any(Book.class));
     }
 }
