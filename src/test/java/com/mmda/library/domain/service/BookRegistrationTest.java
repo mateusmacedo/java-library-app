27977b8174b430b3e6ae35625f04d6e854a7726e
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-05-30 09:35:35 -0300

    test(service): :test_tube: expanded book registration test to include detailed dto testing

--- src/test/java/com/mmda/library/domain/service/BookRegistrationTest.java
+++ src/test/java/com/mmda/library/domain/service/BookRegistrationTest.java
@@ -3,6 +3,8 @@ package com.mmda.library.domain.service;
 import static org.junit.Assert.assertThrows;
 import static org.mockito.Mockito.doThrow;
 
+import java.util.HashSet;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -10,11 +12,15 @@ import org.mockito.MockitoAnnotations;
 
 import com.mmda.core.validation.ValidationException;
 import com.mmda.core.validation.Validator;
+import com.mmda.library.domain.dto.BookRegistrationDto;
+import com.mmda.library.domain.model.Author;
+import com.mmda.library.domain.valueobject.Category;
+import com.mmda.library.domain.valueobject.Isbn;
 
 public class BookRegistrationTest {
 
     @Mock
-    private Validator<Object> validator;
+    private Validator<BookRegistrationDto> validator;
 
     @Before
     public void setUp() {
@@ -23,8 +29,28 @@ public class BookRegistrationTest {
 
     @Test
     public void registerBook_shouldReturnValidationException_whenDtoIsInvalid() {
+        String title = "Title";
+        Author author = new Author("Name", "Surname");
+
+        HashSet<Author> authors = new HashSet<>();
+        authors.add(author);
+
+        Isbn isbn = new Isbn("1234567890123");
+        Category category = new Category("Category");
+
+        HashSet<Category> categories = new HashSet<>();
+        categories.add(category);
+
+        Integer publicationYear = 2021;
+
+        BookRegistrationDto dto = new BookRegistrationDto(
+                title,
+                authors,
+                isbn,
+                categories,
+                publicationYear);
+
         BookRegistration bookRegistration = new BookRegistration(validator);
-        Object dto = new Object();
 
         doThrow(new ValidationException("Invalid DTO")).when(validator).validate(dto);
 
