09409be4fecd29bf18ddf6b1d88a672f922ddc56
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-05-30 11:56:11 -0300

    test(BookRegistration): :test_tube: extended unit tests for handling exceptions and verifying functions
    Enhanced unit tests for the BookRegistration class. Expanded the scope of the testing suite to cover scenarios where the Factory or Repository operations fail, and to verify that they perform as expected during normal operation.

--- src/test/java/com/mmda/library/domain/service/BookRegistrationTest.java
+++ src/test/java/com/mmda/library/domain/service/BookRegistrationTest.java
@@ -18,6 +18,8 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.mmda.core.factory.Factory;
+import com.mmda.core.factory.FactoryException;
 import com.mmda.core.repository.PersistenceRepository;
 import com.mmda.core.validation.ValidationException;
 import com.mmda.core.validation.Validator;
@@ -36,12 +38,15 @@ public class BookRegistrationTest {
     @Mock
     private PersistenceRepository<Book> repository;
 
+    @Mock
+    private Factory<Book> factory;
+
     @InjectMocks
     private BookRegistration bookRegistration;
 
     @BeforeEach
     public void setUp() {
-        bookRegistration = new BookRegistration(validator, repository);
+        bookRegistration = new BookRegistration(validator, repository, factory);
     }
 
     @AfterEach
@@ -72,7 +77,68 @@ public class BookRegistrationTest {
 
         assertEquals("Invalid DTO", exception.getMessage());
         verify(validator).validate(dto);
+        verify(factory, never()).create(any(String.class), any(HashSet.class), any(Isbn.class),
+                any(HashSet.class), any(Integer.class));
         verify(repository, never()).save(any(Book.class));
+    }
+
+    @Test
+    public void registerBook_shouldReturnFactoryException_whenFactoryFails() {
+        String title = "Title";
+        Author author = new Author("Name", "Surname");
+        HashSet<Author> authors = new HashSet<>();
+        authors.add(author);
+        Isbn isbn = new Isbn("1234567890123");
+        Category category = new Category("Category");
+        HashSet<Category> categories = new HashSet<>();
+        categories.add(category);
+        Integer publicationYear = 2021;
+
+        BookRegistrationDto dto = new BookRegistrationDto(
+                title, authors, isbn, categories, publicationYear);
+
+        doNothing().when(validator).validate(any(BookRegistrationDto.class));
+        when(factory.create(any(String.class), any(HashSet.class), any(Isbn.class),
+                any(HashSet.class), any(Integer.class))).thenThrow(new FactoryException("Factory error"));
+
+        RuntimeException exception = assertThrows(FactoryException.class,
+                () -> bookRegistration.registerBook(dto));
+
+        assertEquals("Factory error", exception.getMessage());
+        verify(validator).validate(dto);
+        verify(factory).create(title, authors, isbn, categories, publicationYear);
+        verify(repository, never()).save(any(Book.class));
+    }
+
+    @Test
+    public void registerBook_shouldReturnPersistenceException_whenRepositoryFails() {
+        String title = "Title";
+        Author author = new Author("Name", "Surname");
+        HashSet<Author> authors = new HashSet<>();
+        authors.add(author);
+        Isbn isbn = new Isbn("1234567890123");
+        Category category = new Category("Category");
+        HashSet<Category> categories = new HashSet<>();
+        categories.add(category);
+        Integer publicationYear = 2021;
+
+        BookRegistrationDto dto = new BookRegistrationDto(
+                title, authors, isbn, categories, publicationYear);
+        Book expectedBook = new Book(
+                title, authors, isbn, categories, publicationYear);
+
+        doNothing().when(validator).validate(any(BookRegistrationDto.class));
+        when(factory.create(any(String.class), any(HashSet.class), any(Isbn.class),
+                any(HashSet.class), any(Integer.class))).thenReturn(expectedBook);
+        when(repository.save(any(Book.class))).thenThrow(new RuntimeException("Repository error"));
+
+        RuntimeException exception = assertThrows(RuntimeException.class,
+                () -> bookRegistration.registerBook(dto));
+
+        assertEquals("Repository error", exception.getMessage());
+        verify(validator).validate(dto);
+        verify(factory).create(title, authors, isbn, categories, publicationYear);
+        verify(repository).save(expectedBook);
     }
 
     @Test
@@ -87,18 +153,21 @@ public class BookRegistrationTest {
         categories.add(category);
         Integer publicationYear = 2021;
 
-        Book expectedBook = new Book(
-                title, authors, isbn, categories, publicationYear);
         BookRegistrationDto dto = new BookRegistrationDto(
+                title, authors, isbn, categories, publicationYear);
+        Book expectedBook = new Book(
                 title, authors, isbn, categories, publicationYear);
 
         doNothing().when(validator).validate(any(BookRegistrationDto.class));
+        when(factory.create(any(String.class), any(HashSet.class), any(Isbn.class),
+                any(HashSet.class), any(Integer.class))).thenReturn(expectedBook);
         when(repository.save(any(Book.class))).thenReturn(expectedBook);
 
         Book resultBook = bookRegistration.registerBook(dto);
 
         assertEquals(expectedBook, resultBook);
         verify(validator).validate(dto);
-        verify(repository).save(any(Book.class));
+        verify(factory).create(title, authors, isbn, categories, publicationYear);
+        verify(repository).save(expectedBook);
     }
 }
